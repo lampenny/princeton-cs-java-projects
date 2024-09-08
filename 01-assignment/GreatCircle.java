aae097ed1207aee8529863c93607dfcf362fe42c
Penny L <pennylam@live.com>
2024-09-08 19:33:51 +0900

    Fix GreatCircle to use only Haversine formula

--- 01-assignment/GreatCircle.java
+++ 01-assignment/GreatCircle.java
@@ -9,36 +9,20 @@ public class GreatCircle {
         double y2 = Math.toRadians(Double.parseDouble(args[3]));
 
        /*************************************************************************
-        * Compute using law of cosines
-        *************************************************************************/
-        // great circle distance in radians
-        double angle1 = Math.acos(Math.sin(x1) * Math.sin(x2)
-                      + Math.cos(x1) * Math.cos(x2) * Math.cos(y1 - y2));
-
-        // convert back to degrees
-        angle1 = Math.toDegrees(angle1);
-
-        // each degree on a great circle of Earth is 60 nautical miles
-        double distance1 = 60 * angle1;
-
-        System.out.println(distance1 + " nautical miles");
-
-
-       /*************************************************************************
         * Compute using Haversine formula
         *************************************************************************/
         double a = Math.pow(Math.sin((x2-x1)/2), 2)
                  + Math.cos(x1) * Math.cos(x2) * Math.pow(Math.sin((y2-y1)/2), 2);
 
         // great circle distance in radians
-        double angle2 = 2 * Math.asin(Math.min(1, Math.sqrt(a)));
+        double angle = 2 * Math.asin(Math.min(1, Math.sqrt(a)));
 
         // convert back to degrees
-        angle2 = Math.toDegrees(angle2);
+        angle = Math.toDegrees(angle);
 
         // each degree on a great circle of Earth is 60 nautical miles
-        double distance2 = 60 * angle2;
+        double distance = 60 * angle;
 
-        System.out.println(distance2 + " nautical miles");
+        System.out.println(distance + " kilometers");
     }
 }
