9716179ae2069ffd2077b2e5c72ee971d3bcc25d
Penny L <pennylam@live.com>
2024-09-28 17:59:48 +0900

    Add minesweeper and discrete distribution solution

--- 03-arrays/DiscreteDistribution.java
+++ 03-arrays/DiscreteDistribution.java
@@ -1,3 +1,37 @@
-public class DiscreteDistribution {
-    
+public class DiscreteDistribution
+{
+    public static void main(String[] args)
+    {
+        int m = Integer.parseInt(args[0]);
+        int cumlativeSum = 0;
+        int[] s = new int[args.length + 1];
+
+        for (int i = 1; i < args.length; i++)
+        {
+            cumlativeSum += Integer.parseInt(args[i]);
+        }
+
+        s[0] = 0;
+
+        for (int i = 1; i < args.length; i++)
+        {
+            s[i] = Integer.parseInt(args[i]) + s[i - 1];
+        }
+
+        s[args.length] = s[args.length - 1] + 1;
+        for (int i = 0; i < m; i++)
+        {
+            double random = cumlativeSum * Math.random();
+            int randi = (int) random;
+
+            for (int j = 0; j < args.length; j++)
+            {
+                if (randi >= s[j] && randi < s[j + 1]) {
+                    System.out.print(j + 1 + " ");
+                    break;
+                }
+            }
+        }
+        System.out.println();
+    }
 }
--- 03-arrays/Minesweeper.java
+++ 03-arrays/Minesweeper.java
@@ -1,3 +1,51 @@
-public class Minesweeper {
-    
+public class Minesweeper
+{
+    public static void main(String[] args)
+    {
+        int m = Integer.parseInt(args[0]);
+        int n = Integer.parseInt(args[1]);
+        int k = Integer.parseInt(args[2]);
+
+        int[][] grid = new int[m][n];
+        int[] mines = new int[k * 2];
+
+        double mathRandom = Math.random();
+
+        // Generate the coordinates of minus in the array
+        int coordinate = 0;
+        while (k * 2 > coordinate) {
+            int a = (int) (mathRandom * m), b = (int) (mathRandom * n);
+            if (grid[a][b] == 0) {
+                grid[a][b] = -1;
+
+                mines[coordinate] = a;
+                mines[coordinate + 1] = b;
+                coordinate += 2;
+            }
+        }
+
+        // Increase the number of neighboring mines
+        for (int i = 0; i < k * 2; i += 2) {
+            for (int a = -1; a < 2; a++) {
+                if (mines[i] + a >= 0 && mines[i] + a < m) {
+                    for (int b = -1; b < 2; b++) {
+                        if (mines[i + 1] + b >= 0 && mines[i + 1] + b < n && grid[mines[i] + a][mines[i + 1] + b] >= 0) {
+                            grid[mines[i] + a][mines[i + 1] + b]++;
+                        }
+                    }
+                }
+            }
+        }
+
+        // Print the grid
+        for (
+                int i = 0;
+                i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] >= 0) System.out.print(grid[i][j] + "  ");
+                else System.out.print("*  ");
+            }
+            System.out.println();
+        }
+    }
 }
