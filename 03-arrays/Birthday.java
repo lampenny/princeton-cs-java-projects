080da5798259edab5121e588cb3087fff66323b1
Penny L <pennylam@live.com>
2024-09-28 17:57:43 +0900

    Add Birthday and ThueMorse solutions

--- 03-arrays/Birthday.java
+++ 03-arrays/Birthday.java
@@ -1,3 +1,39 @@
-public class Birthday {
-    
+public class Birthday
+{
+    public static void main(String[] args)
+    {
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+
+        int[] count = new int[n];
+        double[] fraction = new double[n];
+        fraction[0] = 0;
+
+        for (int t = 0; t < trials; t++)
+        {
+            boolean[] room = new boolean[n];
+            for (int i = 0; i < n; i++)
+            {
+                int birthday = (int) (Math.random() * (n - 1));
+                if (!room[birthday])
+                {
+                    room[birthday] = true;
+                } else
+                {
+                    count[i]++;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i > 0)
+            {
+                fraction[i] = fraction[i - 1] + ((double) count[i] / (double) trials);
+            }
+            System.out.println((i + 1) + "\t" + count[i] + "\t" + fraction[i]);
+            if (fraction[i] >= 0.5) break;
+        }
+    }
 }
--- 03-arrays/ThueMorse.java
+++ 03-arrays/ThueMorse.java
@@ -1,3 +1,30 @@
 public class ThueMorse {
-    
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+
+        // Create a boolean array of length n.
+        int[] sequence = new int[n];
+
+        // Initialize the first bit in the array to false.
+        sequence[0] = 0;
+
+        // Generates the thue morse sequence
+        for (int i = 1; i < n; i++) {
+            if (i % 2 == 0) // checks if i is power of 2
+                sequence[i] = sequence[i / 2];
+            else
+                sequence[i] = 1 - sequence[i - 1];
+        }
+
+        // Prints the sequence matrix using a double nested loop
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (sequence[i] == sequence[j])
+                    System.out.print("+  ");
+                else
+                    System.out.print("-  ");
+            }
+            System.out.println();
+        }
+    }
 }
