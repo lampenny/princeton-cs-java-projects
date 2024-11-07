bb3015484d660dfafec1ad68d9bc7e64e1ce6df6
Penny L <pennylam@live.com>
2024-11-07 17:56:00 +0900

    Add Shannon Entropy

---
+++ 04-input-output/README.md
@@ -0,0 +1,9 @@
+# INPUT AND OUTPUT
+
+The purpose of these assignments are to give you practice writing programs with input and output, including standard output, standard input, and standard drawing.
+
+### ShannonEntropy.java
+
+The Shannon entropy is a measure of the rate of information produced by a random source, such as the outcomes of flipping a fair coin or rolling a loaded die. It is a fundamental concept in information theory and data compression.
+
+A higher entropy value indicates greater randomness.
--- 04-input-output/ShannonEntropy.java
+++ 04-input-output/ShannonEntropy.java
@@ -1,3 +1,20 @@
 public class ShannonEntropy {
+	public static void main(String[] args) {
+		int m = Integer.parseInt(args[0]);
+		int[] frequencies = new int[m];
+		double count = 0;
+		double entropy = 0.0;
+		double log2 = Math.log(2);
+		int[] sequence = StdIn.readAllInts();
 
+		for (int i = 0; i < sequence.length; i++)
+			frequencies[sequence[i] - 1]++;
+
+		for (int i = 0; i < frequencies.length; i++) {
+			double p = (double) frequencies[i] / (double) sequence.length;
+			entropy += p == 0 ? 0 : -p * Math.log(p) / log2;
+		}
+
+		StdOut.printf("%.4f\n", entropy);
+	}
 }
