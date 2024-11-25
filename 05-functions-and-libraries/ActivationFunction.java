5b20a1af9dbcf5b45a4dd9e96610f394daeff146
Penny L <pennylam@live.com>
2024-11-25 20:35:17 +0900

    add 3 out 4 functions

--- 05-functions-and-libraries/ActivationFunction.java
+++ 05-functions-and-libraries/ActivationFunction.java
@@ -1,32 +1,52 @@
 public class ActivationFunction {
-	// Returns the Heaviside function of x.
-	public static double heaviside(double x) {
+  // An activation function is a function that maps real numbers
+  // into a desired range, such as between 0 and 1 or between –1 and +1.
 
+  // Returns the Heaviside function of x.
+  public static double heaviside(double x) {
+    if (Double.isNaN(x)) { return Double.NaN };
+
+    if (x < 0) {
+      return 0;
+		} else if (x == 0) {
+      return 0.5;
+    } else {
+      return 1;
+    }
 	}
 
-	// Returns the sigmoid function of x.
-	public static double sigmoid(double x) {
+  // Returns the sigmoid function of x.
+  public static double sigmoid(double x) {
+    if (Double.isNaN(x)) { return Double.NaN };
 
-	}
+    return 1 / (1 + Math.exp(-x));
+  }
 
-	// Returns the hyperbolic tangent of x.
-	public static double tanh(double x) {
+  // Returns the hyperbolic tangent of x.
+  public static double tanh(double x) {
+    if (Double.isNaN(x)) { return Double.NaN };
 
-	}
+    return (Math.exp(x) - Math.exp(-x)) / (Math.exp(x) + Math.exp(-x))
+  }
 
-	// Returns the softsign function of x.
-	public static double softsign(double x) {
+  // Returns the softsign function of x.
+  public static double softsign(double x) {
+    if (Double.isNaN(x)) { return Double.NaN };
 
-	}
+    if (x == Double.POSITIVE_INFINITY) { return 1; }
+    if (x == Double.NEGATIVE_INFINITY) { return -1; }
 
-	// Returns the square nonlinearity function of x.
-	public static double sqnl(double x) {
+    return x / (1 + Math.abs(x))
+  }
 
-	}
+  // Returns the square nonlinearity function of x.
+  public static double sqnl(double x) {
 
-	// Takes a double command-line argument x and prints each activation
-	// function, evaluated, in the format (and order) given below.
-	public static void main(String[] args) {
+  }
 
-	}
+  // Takes a double command-line argument x and prints each activation
+  // function, evaluated, in the format (and order) given below.
+  public static void main(String[] args) {
+
+  }
 }
